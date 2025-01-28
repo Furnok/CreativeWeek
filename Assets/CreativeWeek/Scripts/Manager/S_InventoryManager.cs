d9a19b606ad903842273c691d010d678c4a3dd75
MartinsFlorian <f.martins2005.fm@gmail.com>
2025-01-28 17:18:05 +0100

    S_InventoryManager

---
+++ Assets/CreativeWeek/Scripts/Manager/S_InventoryManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class S_InventoryManager : MonoBehaviour
+{
+    [Header("RSE")]
+    [SerializeField] RSE_PickUpItem RSE_PickUpItem;
+    [SerializeField] RSE_RemoveItem RSE_RemoveItem;
+    [Header("RSO")]
+    [SerializeField] RSO_CurrentListObject RSO_CurrentListObject;
+    [Header("SSO")]
+    [SerializeField] SSO_ListObject SSO_ListObject;
+    [SerializeField] SSO_MaxObject SSO_MaxObject;
+    private bool isNotInCurrentList = true;
+    private bool isInCurrentList = false;
+    private void OnEnable()
+    {
+        RSE_PickUpItem.action += AddItem;
+        RSE_RemoveItem.action += RemoveItem;
+    }
+    private void OnDisable()
+    {
+        RSE_PickUpItem.action -= AddItem;
+        RSE_RemoveItem.action -= RemoveItem;
+    }
+    private void AddItem(int index)
+    {
+        if(RSO_CurrentListObject.Value.Count < SSO_MaxObject.Value)
+        {
+            isNotInCurrentList = true;
+            foreach(var item in RSO_CurrentListObject.Value)
+            {
+                if(item.Index == index)
+                {
+                    isNotInCurrentList = false;
+                }
+            }
+            if (isNotInCurrentList)
+            {
+                RSO_CurrentListObject.Value.Add(SSO_ListObject.Value.FirstOrDefault(x=> x.Index == index));
+            }
+        }
+    }
+    private void RemoveItem(int index)
+    {
+        if(RSO_CurrentListObject.Value.Count > 0)
+        {
+            isInCurrentList = false;
+            foreach (var item in RSO_CurrentListObject.Value)
+            {
+                if (item.Index == index)
+                {
+                    isInCurrentList = true;
+                }
+            }
+            if (isInCurrentList)
+            {
+                RSO_CurrentListObject.Value.Remove(SSO_ListObject.Value.FirstOrDefault(x => x.Index == index));
+            }
+            
+        }
+    }
+}
---
+++ Assets/CreativeWeek/Scripts/RSE/RSE_RemoveItem.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_RemoveItem", menuName = "Data/RSE/Item/RemoveItem")]
+public class RSE_RemoveItem : RuntimeScriptableEvent<int>
+{
+}
