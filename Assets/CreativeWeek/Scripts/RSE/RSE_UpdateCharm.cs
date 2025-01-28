52ccb801a473175d90b8aa5ae0ef2353fd596a12
MartinsFlorian <f.martins2005.fm@gmail.com>
2025-01-28 16:03:09 +0100

    Create RSE + data +Prefabs CharmManager

---
+++ Assets/CreativeWeek/Scripts/RSE/RSE_UpdateCharm.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_UpdateCharm", menuName = "Data/RSE/Charm/UpdateCharm")]
+public class RSE_UpdateCharm : RuntimeScriptableEvent<int>
+{
+}
