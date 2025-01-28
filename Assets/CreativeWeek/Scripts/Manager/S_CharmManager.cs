99db8416610b839c070d3aa95fe2e6f843900caa
MartinsFlorian <f.martins2005.fm@gmail.com>
2025-01-28 15:20:01 +0100

    S_CharmManager

---
+++ Assets/CreativeWeek/Scripts/Manager/S_CharmManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_CharmManager : MonoBehaviour
+{
+    [Header("SSO")]
+    [SerializeField] SSO_Charm SSO_Charm;
+    [Header("RSO")]
+    [SerializeField] RSO_Charm RSO_Charm;
+}
