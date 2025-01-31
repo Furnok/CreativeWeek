fff5b4077020825564620ca2be43c4d456edd112
Furnok <killian.furno@orange.fr>
2025-01-31 11:46:17 +0100

    Fix

--- Assets/CreativeWeek/Scripts/ClickableObject/S_ClickableObject.cs
+++ Assets/CreativeWeek/Scripts/ClickableObject/S_ClickableObject.cs
@@ -6,18 +6,25 @@ using UnityEngine.UI;
 
 public class S_ClickableObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] private Material _material;
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        GetComponent<Image>().color = new Color(1, 1, 0, 1f);
-        GetComponent<RectTransform>().localScale = new Vector3(1.2f, 1.2f, 1.2f);
-        transform.GetChild(0).gameObject.SetActive(true);
+        //GetComponent<Image>().color = new Color(1, 1, 0, 1f);
+        //GetComponent<RectTransform>().localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        //transform.GetChild(0).gameObject.SetActive(true);
+
+        GetComponent<Image>().material = _material;
+        _material.SetTexture("_ImageTexture", GetComponent<Image>().sprite.texture);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        GetComponent<Image>().color = Color.white;
-        GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
-        transform.GetChild(0).gameObject.SetActive(false);
+        GetComponent<Image>().material = null;
+
+        //GetComponent<Image>().color = Color.white;
+        //GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
+        //transform.GetChild(0).gameObject.SetActive(false);
     }
 
     private void OnDisable()
