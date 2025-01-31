b4672e2a3d02e6247f1c24242e470a4838860422
MartinsFlorian <f.martins2005.fm@gmail.com>
2025-01-31 10:14:52 +0100

    LaunchPassiveEvent

---
+++ Assets/CreativeWeek/Scripts/RSE/RSE_OnDateStepChange.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_OnDateStepChange", menuName = "Data/RSE/Date/OnDateStepChange")]
+public class RSE_OnDateStepChange : BT.ScriptablesObject.RuntimeScriptableEvent{}
