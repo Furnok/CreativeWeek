281f4f056120f6ed3d3ecc4ccaed065f16728d3f
MartinsFlorian <f.martins2005.fm@gmail.com>
2025-01-31 14:19:17 +0100

    Scene date Toilet

---
+++ Assets/CreativeWeek/Scripts/RSE/RSE_ExitToilet.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_ExitToilet", menuName = "Data/RSE/UI/ExitToilet")]
+public class RSE_ExitToilet : BT.ScriptablesObject.RuntimeScriptableEvent{}
--- Assets/CreativeWeek/Scripts/UI/S_UIToilet.cs
+++ Assets/CreativeWeek/Scripts/UI/S_UIToilet.cs
@@ -4,11 +4,33 @@ public class S_UIToilet : MonoBehaviour
 {
     //[Header("Parameters")]
 
-    //[Header("References")]
+    [Header("References")]
+    [SerializeField] GameObject toiletPanel;
 
-    //[Header("RSE")]
+    [Header("RSE")]
+    [SerializeField] RSE_EnterToilet RSE_EnterToilet;
+    [SerializeField] RSE_ExitToilet RSE_ExitToilet;
 
     //[Header("RSO")]
 
     //[Header("SSO")]
+
+    private void OnEnable()
+    {
+        RSE_EnterToilet.action += EnterToilet;
+        RSE_ExitToilet.action += ExitToilet;
+    }
+    private void OnDisable()
+    {
+        RSE_EnterToilet.action -= EnterToilet;
+        RSE_ExitToilet.action -= ExitToilet;
+    }
+    private void EnterToilet()
+    {
+        toiletPanel.SetActive(true);
+    }
+    private void ExitToilet()
+    {
+        toiletPanel.SetActive(false);
+    }
 }
