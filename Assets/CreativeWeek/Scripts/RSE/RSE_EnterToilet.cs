1d826a0f0902631e242b368de380a94e853bc089
MartinsFlorian <f.martins2005.fm@gmail.com>
2025-01-31 13:42:43 +0100

    ToiletPanel fix prefab

---
+++ Assets/CreativeWeek/Scripts/RSE/RSE_EnterToilet.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_EnterToilet", menuName = "Data/RSE/UI/EnterToilet")]
+public class RSE_EnterToilet : BT.ScriptablesObject.RuntimeScriptableEvent{}
---
+++ Assets/CreativeWeek/Scripts/UI/S_UIToilet.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class S_UIToilet : MonoBehaviour
+{
+    //[Header("Parameters")]
+
+    //[Header("References")]
+
+    //[Header("RSE")]
+
+    //[Header("RSO")]
+
+    //[Header("SSO")]
+}
