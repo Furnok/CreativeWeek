a813e1443ae427bcc095230fa208b86249ad8364
Furnok <killian.furno@orange.fr>
2025-01-31 11:49:07 +0100

    Fix

--- Assets/CreativeWeek/Scripts/ClickableObject/S_ClickableObject.cs
+++ Assets/CreativeWeek/Scripts/ClickableObject/S_ClickableObject.cs
@@ -10,21 +10,23 @@ public class S_ClickableObject : MonoBehaviour, IPointerEnterHandler, IPointerEx
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        //GetComponent<Image>().color = new Color(1, 1, 0, 1f);
-        //GetComponent<RectTransform>().localScale = new Vector3(1.2f, 1.2f, 1.2f);
-        //transform.GetChild(0).gameObject.SetActive(true);
-
         GetComponent<Image>().material = _material;
         _material.SetTexture("_ImageTexture", GetComponent<Image>().sprite.texture);
+
+        GetComponent<RectTransform>().localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        transform.GetChild(0).gameObject.SetActive(true);
+
+        //GetComponent<Image>().color = new Color(1, 1, 0, 1f);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         GetComponent<Image>().material = null;
 
+        GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
+        transform.GetChild(0).gameObject.SetActive(false);
+
         //GetComponent<Image>().color = Color.white;
-        //GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
-        //transform.GetChild(0).gameObject.SetActive(false);
     }
 
     private void OnDisable()
